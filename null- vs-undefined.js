826280b2009ba6463089e8c5f01a2b63c5296fa5
alikhanmohammadsajid <alikhanmohammadsajid@gmail.com>
2021-02-15 01:36:49 +0600

    second commit

---
+++ null- vs-undefined.js
@@ -0,0 +1,7 @@
+// let pakhi;
+function pakhi(num1 ,num2) {
+    console.log(num1 + num2);
+    
+}
+const add = pakhi(13, 4)
+console.log(add);
---
+++ true-false.js
@@ -0,0 +1,8 @@
+const age = 1
+const name = true
+if (name.length) {
+    console.log('condition is true')
+}
+else {
+    console.log('condition is false');
+}
